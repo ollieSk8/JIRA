bd4d2d55894361dc642d9f5fb27f395e70b5a3e5
shichen.liu <shichen.liu@egreatwall.com>
2021-07-19 17:33:52 +0800

    feat: 增加ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import { useAuth } from 'context/auth-context';
 import { ReactElement } from 'react';
 import { AuthenticatedApp } from './authenticated-app';
 import { UnAuthenticatedApp } from './unauthenticated-app';
+import { ErrorBoundary } from 'react-error-boundary';
+import { FullPageErrorFallback } from 'components/lib';
 function App(): ReactElement {
   const { user } = useAuth();
-  return <div>{user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}</div>;
+  return (
+    <ErrorBoundary FallbackComponent={FullPageErrorFallback}>
+      <div>{user ? <AuthenticatedApp /> : <UnAuthenticatedApp />}</div>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { Spin, Typography } from 'antd';
+import { DevTools } from 'jira-dev-tool';
 export const Row = styled.div<{
   gap?: number | boolean;
   between?: boolean;
@@ -42,3 +43,17 @@ export const FullPageError = ({ errormsg }: { errormsg: Error | null }) => {
     </FullPage>
   );
 };
+// 类型守卫
+const isError = (value: any): value is Error => value?.message;
+export const ErrorBox = ({ error }: { error: unknown }) => {
+  if (isError(error)) {
+    return <Typography.Text type={'danger'}>{error?.message}</Typography.Text>;
+  }
+  return null;
+};
+export const FullPageErrorFallback = ({ error }: { error: Error | null }) => (
+  <FullPage>
+    <DevTools />
+    <ErrorBox error={error} />
+  </FullPage>
+);
